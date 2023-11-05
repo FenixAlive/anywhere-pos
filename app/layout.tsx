96cb0b4a7b02bb0fa54a51cc495700b66216a156
FenixAlive <luisangelmf@gmail.com>
2023-11-04 23:14:43 -0600

    starting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import { GeistSans } from 'geist/font'
 import './globals.css'
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000'
-
-export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
-}
-
 export default function RootLayout({
   children,
 }: {
@@ -19,6 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={GeistSans.className}>
       <body className="bg-background text-foreground">
+
         <main className="min-h-screen flex flex-col items-center">
           {children}
         </main>
---
+++ components/Pos.tsx
@@ -0,0 +1,26 @@
+import { createClient } from '@/utils/supabase/server'
+import Link from 'next/link'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+export default async function Pos() {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  return user ? (
+    <div className="flex items-center gap-4">
+      POS
+    </div>
+  ) : (
+    <Link
+      href="/login"
+      className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+    >
+      Login
+    </Link>
+  )
+}
